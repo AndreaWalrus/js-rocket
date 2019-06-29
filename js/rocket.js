ca8c41e7de2646ed92e576cb2ecea31dd3a3d3e7
AndreaWalrus <anprocessing@gmail.com>
2019-06-29 18:55:45 +0200

    finished rocket skeleton

--- js/rocket.js
+++ js/rocket.js
@@ -1,9 +1,35 @@
+var timer = null;
+
+var countdownNum = 10;
+
 var changeState = function (state) {
 	document.body.className = "body-state"+state;
 
+	clearInterval(timer);
+	countdownNum = 10;
+	document.getElementById("countdown").innerHTML = countdownNum;
+
 	if (state==2) {
-		setTimeout(function(){
-		document.getElementById("countdown").innerHTML = document.getElementById("countdown").innerHTML - 1;
-		},1000);
-	}
+		timer = setInterval(function () {
+			countdownNum = countdownNum - 1;
+			document.getElementById("countdown").innerHTML = countdownNum;	
+			if (countdownNum<=0){
+				changeState(3);
+			}
+		}, 500);	
+	}else if(state == 3){
+		var success = setTimeout(function() {
+			var randomNumber = Math.round(Math.random()*10);
+
+			console.log("randomNumber: ", randomNumber);
+
+			//succes
+			if (randomNumber>5) {
+				changeState(4);
+			//failure
+			}else{
+				changeState(5);
+			}
+		}, 2000);
+	};
 }
